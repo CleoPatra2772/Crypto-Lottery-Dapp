b50c167b8cd8e68b4ed3e9db14da70026fe3a7a7
Cleo Gao <celeo127@outlook.com>
2022-11-10 16:33:22 +0800

    implemented count-down timer

---
+++ constants.ts
@@ -0,0 +1,2 @@
+export const currency: string = "MATIC";
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Header } from '../components/Header'
 import { Loading } from '../components/Loading'
+import { ethers } from 'ethers';
 import {
   useContract,
   useMetamask,
   useDisconnect,
   useAddress,
-  //useContractData,
+  useContractRead,
   //useContractCall,
 } from "@thirdweb-dev/react";
 import { Login } from '../components/Login';
 import { useState } from 'react'
+import { currency } from '../constants'
+import { CountdownTimer } from '../components/CountdownTimer'
 
 
 
@@ -22,7 +25,33 @@ const Home: NextPage = () => {
   const { contract, isLoading } = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
   const [quantity, setQuantity] = useState<number>(1);
 
- 
+  const { data: expiration } = useContractRead(
+    contract,
+    "expiration"
+  );
+
+  const { data: remainingTickets } = useContractRead(
+    contract,
+    "remainingTickets"
+  );
+
+  const { data: currentWinningReward } = useContractRead(
+    contract,
+    "currentWinningReward"
+  );
+
+  const { data: ticketPrice } = useContractRead(
+    contract,
+    "ticketPrice"
+  );
+
+  const { data: ticketCommission } = useContractRead(
+    contract,
+    "ticketCommission"
+  );
+
+
+
   if(isLoading) return <Loading />
 
   if (!address) return <Login />
@@ -41,29 +70,35 @@ const Home: NextPage = () => {
       <Header />
 
 {/* Draw Box */}
-<div className='space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5 max-w-6xl'>
+<div className='space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5 '>
   <div className='stats-container'>
     <h1 className='text-5xl text-white font-semibold text-center'>The Next Draw</h1>
   
   <div className='flex justify-between p-2 space-x-2'>
     <div className='stats'>
     <h2 className='text-sm'>Total Pool</h2>
-    <p className='text-xl'>0.1 MATIC</p>
+    <p className='text-xl'>{currentWinningReward && 
+    ethers.utils.formatEther(currentWinningReward.toString())}{" "}
+    {currency}</p>
   </div>
   <div className="stats">
     <h2 className='text-sm'>Tickets Remaining</h2>
-    <p className='text-xl'>100</p>
+    <p className='text-xl'>{remainingTickets?.toNumber()}</p>
   </div>
     </div>
 
     {/* Timer */}
+    <div className='mt-5 mb-3'>
+    <CountdownTimer />
+    </div>
   </div>
 
   <div className='stats-container space-y-2'>
     <div className='stats-container'>
       <div className='flex justify-between items-center text-white pb-2'>
         <h2>Price per ticket</h2>
-        <p>0.01 MATIC</p>
+        <p>{ticketPrice &&
+        ethers.utils.formatEther(ticketPrice.toString())}{" "}</p>
       </div>
       
       <div className='flex text-white items-center space-x-2 bg-[#091B18] border-[#004337] border p-4'>
@@ -80,12 +115,18 @@ const Home: NextPage = () => {
       <div className='space-y-2 mt-5'>
         <div className='flex items-center justify-between text-emerald-300 text-sm italic font-extrabold'>
         <p>Total cost of tickets</p>
-        <p>0.999</p>
+        <p>
+          {ticketPrice && 
+          Number(
+          ethers.utils.formatEther(ticketPrice.toString())) * quantity} {" "}
+        {currency}
+        </p>
         </div>
 
         <div className='flex items-center justify-between text-emerald-300 text-xs italic'>
           <p>Serive Fee</p>
-          <p>0.001 MATIC</p>
+          <p>{ticketCommission &&
+        ethers.utils.formatEther(ticketCommission.toString())}{" "} {currency}</p>
         </div>
 
         <div className='flex items-center justify-between text-emerald-300 text-xs italic'>
@@ -94,7 +135,9 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <button  className='mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md
+      <button
+      disabled={expiration?.toString() < Date.now().toString() || remainingTickets?.toNumber() === 0  }
+        className='mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md
       text-white shadow-xl disabled:from-gray-600 disabled:text-gray-100 disabled:to-gray-600 disabled:cursor-not-allowed '>Buy tickets</button>
     </div>
   </div>
