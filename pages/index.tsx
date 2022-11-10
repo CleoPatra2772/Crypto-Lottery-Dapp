8a77d464c9d0b6ee4d7bc1a2e48e453c6e6d3a12
Cleo Gao <celeo127@outlook.com>
2022-11-10 20:23:14 +0800

    added ticket box to show user their tickets

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,13 @@ import {
   useDisconnect,
   useAddress,
   useContractRead,
-  //useContractCall,
+  useContractWrite,
 } from "@thirdweb-dev/react";
 import { Login } from '../components/Login';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { currency } from '../constants'
 import { CountdownTimer } from '../components/CountdownTimer'
-
+import toast from 'react-hot-toast';
 
 
 const Home: NextPage = () => {
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
   const address = useAddress();
   const { contract, isLoading } = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
   const [quantity, setQuantity] = useState<number>(1);
-
+  const [userTickets, setUserTickets] = useState(0);
   const { data: expiration } = useContractRead(
     contract,
     "expiration"
@@ -32,12 +32,12 @@ const Home: NextPage = () => {
 
   const { data: remainingTickets } = useContractRead(
     contract,
-    "remainingTickets"
+    "RemainingTickets"
   );
 
   const { data: currentWinningReward } = useContractRead(
     contract,
-    "currentWinningReward"
+    "CurrentWinningReward"
   );
 
   const { data: ticketPrice } = useContractRead(
@@ -49,6 +49,51 @@ const Home: NextPage = () => {
     contract,
     "ticketCommission"
   );
+
+  const { mutateAsync: BuyTickets } = useContractWrite(contract, "BuyTickets");
+
+  const { data: tickets } = useContractRead(
+    contract,
+    "getTickets"
+  );
+
+  useEffect(() => {
+    if(!tickets) return;
+
+    const totalTickets: string[] = tickets;
+
+    const noOfUserTickets = totalTickets.reduce((total, ticketAddress) => (
+      ticketAddress === address ? total + 1 : total
+    ), 0);
+
+    setUserTickets(noOfUserTickets);
+  },[tickets, address]);
+
+  console.log(userTickets);
+
+  const handleClick = async () => {
+    if(!ticketPrice) return;
+    const notification = toast.loading("Buying your tickets...");
+
+    try{
+      const data = await BuyTickets ([
+        {
+        value: ethers.utils.parseEther(
+          (Number(ethers.utils.formatEther(ticketPrice))* quantity).toString()
+        ),
+      }]);
+
+      toast.success("Tickets purchased successfully!", {
+        id: notification,
+      });
+
+    }catch(err){
+      toast.error("Whoops, something went wrong!", {
+        id: notification,
+      });
+      console.error("contract call failure", err);
+    }
+  }
 
 
 
@@ -137,9 +182,26 @@ const Home: NextPage = () => {
 
       <button
       disabled={expiration?.toString() < Date.now().toString() || remainingTickets?.toNumber() === 0  }
-        className='mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md
-      text-white shadow-xl disabled:from-gray-600 disabled:text-gray-100 disabled:to-gray-600 disabled:cursor-not-allowed '>Buy tickets</button>
+          onClick={handleClick}
+        className='mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md font-semibold
+      text-white shadow-xl disabled:from-gray-600 disabled:text-gray-100 disabled:to-gray-600 disabled:cursor-not-allowed '>
+        Buy {quantity} tickets fro {ticketPrice &&
+        Number(ethers.utils.formatEther(ticketPrice.toString())) * quantity} {" "} {currency} 
+        </button>
     </div>
+          {userTickets > 0 && (
+            <div className='stats'>
+              <p className='text-lg mb-2'>you have {userTickets} Tickets in this draw</p>
+            
+            <div className='flex max-w-sm flex-wrap gap-x-2 gap-y-2'>
+              {Array(userTickets).fill("") 
+              .map((_, index) => (
+                <p key={index} className="text-emerald-300 h-20 w-12 bg-emerald-500/30 rounded-lg flex flex-shrink-0 items-center justify-center text-xs italic">{index + 1}</p>
+              ))}
+            </div>
+            </div>
+          )}
+
   </div>
 </div>
 
